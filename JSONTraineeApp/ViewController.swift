213c5afb03988580038bc037aff18984c52759e8
Shalenikoff <KShalen@gmail.com>
2023-09-02 01:57:47 +0300

    Initial Commit

---
+++ JSONTraineeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONTraineeApp
+//
+//  Created by Кирилл Шалеников on 02.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
