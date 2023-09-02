97dd7e890447351a0e28fe2c3a6b1b7e4624f2e9
Shalenikoff <KShalen@gmail.com>
2023-09-02 20:26:54 +0300

    Made UI for MainViewController

---
+++ JSONTraineeApp/Lyfecycle/UserActionCell.swift
@@ -0,0 +1,13 @@
+//
+//  UserActionCell.swift
+//  JSONTraineeApp
+//
+//  Created by Кирилл Шалеников on 02.09.2023.
+//
+
+import UIKit
+
+class UserActionCell: UICollectionViewCell {
+    @IBOutlet var userActionCell: UILabel!
+    
+}
--- JSONTraineeApp/ViewControllers/ViewController.swift
+++ JSONTraineeApp/ViewControllers/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UICollectionViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // MARK: UICollectionViewDataSource
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: "JSONButton",
+                for: indexPath) as? UserActionCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+    
 
 
+}
+
+// MARK: UICollectionViewDelegateFromLayout
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+            CGSize(width: UIScreen.main.bounds.width - 50, height: 100)
+    }
+}
+
+// MARK: Networking
+extension MainViewController {
+    private func rickAndMorty() {
+        
+    }
+    
+    private func calendar() {
+        
+    }
 }
 
