f25d3f61650248f3f1b8e54599abca4775d23062
Shalenikoff <KShalen@gmail.com>
2023-09-02 20:51:29 +0300

    Find the firs API and tryed to make it work

---
+++ JSONTraineeApp/Model.swift
@@ -0,0 +1,10 @@
+//
+//  Model.swift
+//  JSONTraineeApp
+//
+//  Created by Кирилл Шалеников on 02.09.2023.
+//
+
+struct Person: Decodable {
+    
+}
--- JSONTraineeApp/ViewControllers/ViewController.swift
+++ JSONTraineeApp/ViewControllers/ViewController.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 
+enum UserAction: String, CaseIterable {
+    case rickAndMorty = "Rick & Morty"
+}
+
+enum Link: String {
+    case rickAndMorty = "https://rickandmortyapi.com/api/character/108"
+}
+
 class MainViewController: UICollectionViewController {
 
+    let userActions = UserAction.allCases
+    
     // MARK: UICollectionViewDataSource
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        1
+        userActions.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -22,7 +32,19 @@ class MainViewController: UICollectionViewController {
             return UICollectionViewCell()
         }
         
+        cell.userActionCell.text = userActions[indexPath.item].rawValue
+        
         return cell
+    }
+    
+    // MARK: UICollectionViewCellDelegate
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let userAction = userActions[indexPath.item]
+        
+        switch userAction {
+        case .rickAndMorty: rickAndMorty()
+        }
     }
     
 
@@ -41,7 +63,23 @@ extension MainViewController: UICollectionViewDelegateFlowLayout {
 // MARK: Networking
 extension MainViewController {
     private func rickAndMorty() {
+        guard let url = URL(string: Link.rickAndMorty.rawValue) else { return }
         
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error discription")
+                return
+            }
+            
+            do {
+                let person = try JSONDecoder().decode(Person.self, from: data)
+                
+                
+            }
+            
+            
+            
+        }
     }
     
     private func calendar() {
